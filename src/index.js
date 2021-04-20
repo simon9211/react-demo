b4f4c7d6b6203d317af21eddf0cf60183fae2909
Simon <simon9211@126.com>
2021-04-20 17:15:07 +0800

    babel-loader reback

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import App from './App'
-import { AppContainer } from 'react-hot-loader'
+// import { AppContainer } from 'react-hot-loader'
 
 const renderDom = Item => {
 	render(
-		<AppContainer>
-			<Item />
-		</AppContainer>,
+		// <AppContainer>
+			<Item />,
+		// </AppContainer>,
 		document.getElementById('app')
 	);
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         // use: {
         //   loader: 'babel-loader',
         //   options: {
-        //     presets: ["@babel/preset-env", "@babel/preset-react"],
+        //     presets: [ "es2015",  "react"],
         //   }
         // },
         exclude: /node_modules/
